85b9a073db8662fb6f7f738bfcc3e00829a38e1d
Ratik Jindal <ratikjindal21@gmail.com>
2022-11-16 01:29:06 +0530

    use nodev16

--- spawn-nodes/scripts/1-start-node.sh
+++ spawn-nodes/scripts/1-start-node.sh
@@ -17,8 +17,8 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
-nvm use node
+nvm install 16
+nvm use 16
 npm install -g yarn
 
 cd zChain/
